24fb717292f631f78b10907105ca7a2fa99814f3
Josh Mann <jlmann72@gmail.com>
2020-11-17 14:39:01 -0500

    Unique strings and input functions completed and working as intended

--- Program.cs
+++ Program.cs
@@ -2,11 +2,35 @@
 
 namespace IsUnique
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static bool uniqueCharacters(string inputString)
+        {
+            for (int firstIndex = 0; firstIndex < inputString.Length; firstIndex++)
+            {
+                for (int secondIndex = firstIndex + 1; secondIndex < inputString.Length; secondIndex++)
+                {
+                    if (inputString[firstIndex] == inputString[secondIndex])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a string");
+            var input = Console.ReadLine();
+
+            if (uniqueCharacters(input) == true)
+            {
+                Console.WriteLine($"The string {input} has all unique characters");
+            }
+            else
+            {
+                Console.WriteLine($"The string {input} has duplicate characters");
+            }
+        }
     }
-  }
 }
